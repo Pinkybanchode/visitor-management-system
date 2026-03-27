5a59eee42aac19f81e325127d3c11f649468429b
Pinkybanchode <43741842+Pinkybanchode@users.noreply.github.com>
2026-03-27 19:54:39 +0530

    Update API baseURL to include '/api' path

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const API = axios.create({
  // baseURL: "http://localhost:4000/api"
- baseURL:`https://visitor-management-system-i849.onrender.com`
+ baseURL:`https://visitor-management-system-i849.onrender.com/api`
 });
 
 API.interceptors.request.use((req) => {
